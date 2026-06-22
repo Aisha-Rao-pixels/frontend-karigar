f2e9c193308957d02046d279b64fea615ce3e2ab
Rishik Kudidhi <rishikk3010@gmail.com>
2026-06-22 17:42:23 +0530

    Delete frontend directory

--- frontend/src/hooks/use-icon-fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Ionicons } from "@expo/vector-icons";
-import { useFonts } from "expo-font";
-
-export const useIconFonts = (): readonly [boolean, Error | null] => {
-  return useFonts({
-    ...Ionicons.font,
-  });
-};
