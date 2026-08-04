bb90463b2a161c885962f603fe4f1e5857ac298c
pixels-source <pixels@aisharao.com>
2026-08-04 16:15:11 +0530

    Update daily_summary_service.py

--- frontend-karigar/Karigar-backend-Render-ready/backend/daily_summary_service.py
+++ frontend-karigar/Karigar-backend-Render-ready/backend/daily_summary_service.py
@@ -28,7 +28,8 @@ from datetime import datetime, timezone, timedelta
 import httpx
 import openpyxl
 import pypdf
-from email_service import generate_profile_pdffrom openpyxl.styles import (
+from email_service import generate_profile_pdf
+from openpyxl.styles import (
     Font, PatternFill, Alignment, Border, Side, GradientFill
 )
 from openpyxl.utils import get_column_letter
